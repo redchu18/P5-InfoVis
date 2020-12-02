87e4b6469dc4badcd91847593840a9328841385b
Casey Wiltsek <casey.wiltsek@gmail.com>
2020-12-01 22:06:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var width = 500;
 var height = 500;
 
-d3.csv("starbucksfoods.csv", function (csv) {
+d3.csv("starbucksfoods.csv", function(csv) {
 
   for (var i = 0; i < csv.length; ++i) {
     csv[i].Fat = Number(csv[i].Fat);
@@ -10,12 +10,6 @@ d3.csv("starbucksfoods.csv", function (csv) {
     csv[i].Protein = Number(csv[i].Protein);
     csv[i].Calories = Number(csv[i].Calories);
   }
-
-  var data = [];
-    
-  data = csv;
-  
-  // console.log(data)
 
   var fatExtent = d3.extent(csv, function (row) {
     return row.Fat;
@@ -62,7 +56,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
   //add scatterplot points
   var circles1 = chart1
     .selectAll("circle")
-    .data(data)
+    .data(csv)
     .enter()
     .append("circle")
     .attr("id", function (d, i) {
@@ -131,6 +125,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
     .on("change", axisHelper);
 
   function axisHelper() {
+
     var xVariable = document.getElementById("xAxis").value;
     var yVariable = document.getElementById("yAxis").value;
 
@@ -170,12 +165,65 @@ d3.csv("starbucksfoods.csv", function (csv) {
         break;
     }
 
+    chart1
+    .append("g")
+    .attr("transform", "translate(0," + (width - 30) + ")")
+    .call(xAxis)
+    .append("text")
+    .attr("class", "label")
+    .attr("x", width - 16)
+    .attr("y", -6)
+    .attr("id", "xAxisElement")
+    .style("text-anchor", "end");
+
+        //Change axis
+        switch (yVariable) {
+          case "Calories":
+            console.log("Calories");
+            var yAxis = d3.axisLeft().scale(caloriesYScale);
+            circles1.data(csv).attr("cy", function (d) { return caloriesYScale(d.Calories); })
+            break;
+          case "Fat":
+            console.log("Fat");
+            var yAxis = d3.axisLeft().scale(fatYScale);
+            circles1.data(csv).attr("cy", function (d) { return fatYScale(d.Fat); })
+            break;
+          case "Carb":
+            console.log("Carb");
+            var yAxis = d3.axisLeft().scale(carbYScale);
+            circles1.data(csv).attr("cy", function (d) { return carbYScale(d.Carb); })
+            break;
+          case "Fiber":
+            console.log("Fiber");
+            var yAxis = d3.axisLeft().scale(fiberYScale);
+            circles1.data(csv).attr("cy", function (d) { return fiberYScale(d.Fiber); })
+            break;
+          case "Protein":
+            console.log("Protein");
+            var yAxis = d3.axisLeft().scale(proteinYScale);
+            circles1.data(csv).attr("cy", function (d) { return proteinYScale(d.Protein); })
+            break;
+        }
+
+    chart1 // or something else that selects the SVG element in your visualizations
+    .append("g") // create a group node
+    .attr("transform", "translate(50, 0)")
+    .call(yAxis)
+    .append("text")
+    .attr("class", "label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .attr("id", "yAxisElement")
+    .style("text-anchor", "end");
+  }
+
     d3.select(document.getElementById("Bakery"))
     .on("click", function() {
         if (document.getElementById("Bakery").checked != true) {
           circles1.filter(function(d) {
             return (d.Category === "Bakery")
-          }).style('fill-opacity', 0).style('stroke', 'white')
+          }).style('fill-opacity', 0).style('stroke-opacity', 0)
         } else {
           circles1.filter(function(d) {
             return (d.Category === "Bakery")
@@ -188,7 +236,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
         if (document.getElementById("Lunch").checked != true) {
           circles1.filter(function(d) {
             return (d.Category === "Lunch")
-          }).style('fill-opacity', 0).style('stroke', 'white')
+          }).style('fill-opacity', 0).style('stroke-opacity', 0)
         } else {
           circles1.filter(function(d) {
             return (d.Category === "Lunch")
@@ -201,7 +249,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
         if (document.getElementById("Hot Breakfast").checked != true) {
           circles1.filter(function(d) {
             return (d.Category === "Hot Breakfast")
-          }).style('fill-opacity', 0).style('stroke', 'white')
+          }).style('fill-opacity', 0).style('stroke-opacity', 0)
         } else {
           circles1.filter(function(d) {
             return (d.Category === "Hot Breakfast")
@@ -214,7 +262,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
         if (document.getElementById("Snacks & Sweets").checked != true) {
           circles1.filter(function(d) {
             return (d.Category === "Snacks & Sweets")
-          }).style('fill-opacity', 0).style('stroke', 'white')
+          }).style('fill-opacity', 0).style('stroke-opacity', 0)
         } else {
           circles1.filter(function(d) {
             return (d.Category === "Snacks & Sweets")
@@ -227,7 +275,7 @@ d3.csv("starbucksfoods.csv", function (csv) {
         if (document.getElementById("Yogurt & Custard").checked != true) {
           circles1.filter(function(d) {
             return (d.Category === "Yogurt & Custard")
-          }).style('fill-opacity', 0).style('stroke', 'white')
+          }).style('fill-opacity', 0).style('stroke-opacity', 0)
         } else {
           circles1.filter(function(d) {
             return (d.Category === "Yogurt & Custard")
